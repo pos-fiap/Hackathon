c113c6c64dde5282b33e4bfb45a7fe60068c17e5
Ricardo Maciel <42289437+Ricardudles@users.noreply.github.com>
2024-08-03 04:33:45 -0300

    .

--- Hackathon.Api/Program.cs
+++ Hackathon.Api/Program.cs
@@ -11,7 +11,7 @@ using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-string connectionString = builder.Configuration.GetConnectionString("DockerfileDefaultConnection") ?? throw new InvalidOperationException("The connection string 'DefaultConnection' is missing or empty."); ;
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("The connection string 'DefaultConnection' is missing or empty."); ;
 
 builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(connectionString, options => options.EnableRetryOnFailure()).UseLazyLoadingProxies());
 
