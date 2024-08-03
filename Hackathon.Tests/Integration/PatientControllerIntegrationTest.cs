09f1034cabc68279cc21356df9cce8a2788e2c31
Ricardo Maciel <42289437+Ricardudles@users.noreply.github.com>
2024-08-03 02:15:50 -0300

    .

--- Hackathon.Tests/Integration/PatientControllerIntegrationTest.cs
+++ Hackathon.Tests/Integration/PatientControllerIntegrationTest.cs
@@ -1,95 +1,98 @@
-﻿//using Hackathon.Api.Controllers;
-//using Hackathon.Application.DTOs;
-//using Hackathon.Application.Services;
-//using Hackathon.Domain.Entities;
-//using Hackathon.Infra.Data.Context;
-//using Hackathon.Infra.Data.Repositories;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.EntityFrameworkCore;
-//using Xunit;
+﻿using Hackathon.Api.Controllers;
+using Hackathon.Infra.Data.Context;
+using Hackathon.Infra.Data.Repositories;
+using Hackathon.Application.Services;
+using Hackathon.Domain.Entities;
+using Xunit;
+using Hackathon.Application.Interfaces;
+using Hackathon.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Hackathon.Application.DTOs;
+using Microsoft.AspNetCore.Mvc;
 
-//namespace Hackathon.Tests.Integration
-//{
-//    [Collection("PatientIntegration")]
-//    public class PatientControllerIntegrationTest : BaseIntegrationTest
-//    {
-//        private DbContextOptions<ApplicationContext> _dbContextOptions;
-//        private ApplicationContext _applicationContext;
-//        private PatientRepository _patientRepository;
-//        private PersonRepository _personRepository;
-//        private PatientService _patientService;        
-//        private PatientController _patientController;
-//        private Patient _patient;
-//        private Person _person;
-        
-//        public PatientControllerIntegrationTest() : base()
-//        {
-//            Configure();
-//        }
+namespace Hackathon.Tests.Integration
+{
+    [Collection("PatientIntegration")]
+    public class PatientControllerIntegrationTest : BaseIntegrationTest
+    {
+        private DbContextOptions<ApplicationContext> _dbContextOptions;
+        private ApplicationContext _applicationContext;
+        private PatientRepository _patientRepository;
+        private PersonRepository _personRepository;
+        private PatientService _patientService;
+        private PatientController _patientController;
+        private Patient _patient;
+        private Person _person;
 
-//        private void Configure()
-//        {
-//            _dbContextOptions = new DbContextOptionsBuilder<ApplicationContext>().UseSqlServer(ConnectionString).Options;
-//            _applicationContext = new ApplicationContext(_dbContextOptions);
-//            _patientRepository = new PatientRepository(_applicationContext);
-//            //_patientService = new PatientService(_patientRepository, _personRepository);
-            
-//        }
+        public PatientControllerIntegrationTest() : base()
+        {
+            Configure();
+        }
 
-//        private void SeedDatabase(ApplicationContext dbContext)
-//        {
-//            CreatePatient(dbContext);
+        private void Configure()
+        {
+            _dbContextOptions = new DbContextOptionsBuilder<ApplicationContext>().UseSqlServer(ConnectionString).Options;
+            _applicationContext = new ApplicationContext(_dbContextOptions);
+            _patientRepository = new PatientRepository(_applicationContext);
+            //_patientService = new PatientService(_patientRepository, _personRepository);
 
-//            dbContext.Patients.Add(_patient);
-//            dbContext.SaveChanges();
-//        }
+        }
 
-//        private void CreatePatient(ApplicationContext dbContext)
-//        {
-//            var existingPatient = dbContext.Patients.FirstOrDefault(b => b.PersonId == 23);
-           
+        private void SeedDatabase(ApplicationContext dbContext)
+        {
+            CreatePatient(dbContext);
 
-//            if (existingPatient is not null)
-//            {
-//                _patient = existingPatient;
-//                return;
-//            }
+            dbContext.Patients.Add(_patient);
+            dbContext.SaveChanges();
+        }
 
-            
-
-//            _person = new Person() { CPF = "1212323", Name="Luana"};
-
-//            _patient = new Patient() { HealthInsuranceNumber = "1111", PersonId = 23, Person = _person};
-//        }
-
-//        private void Delete(ApplicationContext dbContext)
-//        {
-//            var existingPatient = dbContext.Patients.FirstOrDefault(b => b.PersonId == 23);
+        private void CreatePatient(ApplicationContext dbContext)
+        {
+            var existingPatient = dbContext.Patients.FirstOrDefault(b => b.PersonId == 23);
 
 
-//            if (existingPatient != null)
-//            {
-//                dbContext.Patients.Remove(existingPatient);
-//                dbContext.SaveChanges();
-//            }
-               
-//        }
+            if (existingPatient is not null)
+            {
+                _patient = existingPatient;
+                return;
+            }
 
-//        [Fact]
-//        public async Task Add_Returns_CreatedResultWithNewPatient()
-//        {
-//            //Arrange
-//            CreatePatient(_applicationContext);
 
-//            var _patientDto = new PostPatientDto() { HealthInsuranceNumber = _patient.HealthInsuranceNumber };
-//            // Act
-//            var result = await _patientController.Post(_patientDto);
 
-//            // Assert
-//            var createdResult = Assert.IsType<CreatedResult>(result);
+            _person = new Person() { CPF = "1212323", Name = "Luana" };
 
-//            var patient = Assert.IsType<Patient>(createdResult.Value);
-//            Assert.Equal(_patientDto.HealthInsuranceNumber, patient.HealthInsuranceNumber);
+            _patient = new Patient() { HealthInsuranceNumber = "1111", PersonId = 23, Person = _person };
+        }
+
+        private void Delete(ApplicationContext dbContext)
+        {
+            var existingPatient = dbContext.Patients.FirstOrDefault(b => b.PersonId == 23);
+
+
+            if (existingPatient != null)
+            {
+                dbContext.Patients.Remove(existingPatient);
+                dbContext.SaveChanges();
+            }
+
+        }
+
+        [Fact]
+        public async Task Add_Returns_CreatedResultWithNewPatient()
+        {
+            //Arrange
+            CreatePatient(_applicationContext);
+
+            var _patientDto = new PostPatientDto() { HealthInsuranceNumber = _patient.HealthInsuranceNumber };
+            // Act
+            var result = await _patientController.Post(_patientDto);
+
+            // Assert
+            var createdResult = Assert.IsType<CreatedResult>(result);
+
+            var patient = Assert.IsType<Patient>(createdResult.Value);
+            Assert.Equal(_patientDto.HealthInsuranceNumber, patient.HealthInsuranceNumber);
 
             Delete(_applicationContext);
         }
@@ -134,17 +137,17 @@
             // Act
             var result = await _patientController.Put(_patientDto);
 
-//            // Assert
-//            Assert.IsType<OkResult>(result);
+            // Assert
+            Assert.IsType<OkResult>(result);
 
             Delete(_applicationContext);
         }
 
-//        [Fact]
-//        public async Task Remove_Returns_OkResult()
-//        {
-//            // Arrange
-//            SeedDatabase(_applicationContext);
+        [Fact]
+        public async Task Remove_Returns_OkResult()
+        {
+            // Arrange
+            SeedDatabase(_applicationContext);
 
             // Act
             var result = await _patientController.Delete(_patient.Id);
