5858de8ae8fe3b5a2a271f9f6383e0350ce594c5
Luana Lima <luanalima567@outlook.com>
2024-08-02 21:15:53 -0300

    testese

---
+++ Hackathon.Tests/Integration/BaseIntegrationTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Hackathon.Tests.Integration
+{
+    public class BaseIntegrationTest
+    {
+        public IConfigurationRoot Configuration { get; }
+        public static string ConnectionString { get; private set; } = string.Empty;
+        public BaseIntegrationTest()
+        {
+            var configBuilder = new ConfigurationBuilder()
+                                              .SetBasePath(Directory.GetCurrentDirectory())
+                                              .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            Configuration = configBuilder.Build();
+            ConnectionString = Configuration.GetConnectionString("DefaultConnection");
+        }
+    }
+}
